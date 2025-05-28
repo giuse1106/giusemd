89a17b78414fc0d1ec44deebaae374577d6b11db
Giuse <perr.gius11@gmail.com>
2025-05-28 17:09:01 +0200

    Update tools-play.js

--- plugins/tools-play.js
+++ plugins/tools-play.js
@@ -100,7 +100,6 @@ const handleCommand = async (msg, { conn, text, usedPrefix, command }) => {
                     body: "꧁ ĝ̽̓̀͑ỉ͔͖̜͌ư̡͕̭̇s̠҉͍͊ͅẹ̿͋̒̕ẹ̿͋̒̕ ꧂ 「 ᵇᵒᵗ 」",
                     mediaType: 1,
                     previewType: 0,
-                    renderLargerThumbnail: true,
                     mediaUrl: url,
                     sourceUrl: url,
                     thumbnail: thumbData // Puoi anche commentare questa riga se non vuoi l'anteprima del thumbnail
